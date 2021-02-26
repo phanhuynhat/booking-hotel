ff498124368ed9db9292647132354416696b683c
Dell <phanhuynhat1991@gmail.com>
2021-02-26 20:17:30 +0700

    them phong, upload image

--- src/main/java/com/nhat/demo/controller/admin/AdminController.java
+++ src/main/java/com/nhat/demo/controller/admin/AdminController.java
@@ -1,13 +1,32 @@
 package com.nhat.demo.controller.admin;
 
+import com.nhat.demo.entity.Room;
+import com.nhat.demo.entity.RoomType;
+import com.nhat.demo.entity.RoomTypeImage;
+import com.nhat.demo.repository.RoomRepository;
+import com.nhat.demo.repository.RoomTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/admin")
 public class AdminController {
+    public static String uploadDirectory = System.getProperty("user.dir") + "/src/main/resources/static/images/room";
+    @Autowired
+    RoomTypeRepository roomTypeRepository;
+
+    @Autowired
+    RoomRepository roomRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public String showAdminPage() {
@@ -15,8 +34,26 @@ public class AdminController {
     }
 
     @GetMapping("addNewRoom")
-    public String addNewRoom() {
+    public String addNewRoom(Model model) {
+        List<RoomType> roomTypeList = roomTypeRepository.findAll();
+        Room room = new Room();
+        model.addAttribute("room",room);
+        model.addAttribute("roomTypeList",roomTypeList);
         return "/manage/addNewRoom";
+    }
+
+    @PostMapping("saveRoom")
+        public String saveRoom(@ModelAttribute Room room){
+
+        roomRepository.save(room);
+        return "/manage/addNewRoom";
+        }
+
+    @GetMapping("addNewRoomType")
+    public String addNewRoomType(Model model) {
+        RoomType roomType = new RoomType();
+        model.addAttribute("roomType",roomType);
+        return "/manage/addNewRoomType";
     }
 
     @PostMapping("saveNewRoomType")
